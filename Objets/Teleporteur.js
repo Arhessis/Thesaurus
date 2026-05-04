daff15404f5a422ae28cef84b8f91579ed405149
Gogo <george.georgiev04@gmail.com>
2026-05-04 13:03:12 -0400

    creerGeometrieTeleporteur()
    Finished the function

--- Objets/Teleporteur.js
+++ Objets/Teleporteur.js
@@ -9,3 +9,32 @@ function creerTeleporteur(objgl, rgba) {
 
     return objet3D;
 }
+
+function creerGeometrieTeleporteur(objgl) {
+    let buf = objgl.createBuffer();
+
+    const d = 0.283;
+
+    let tab = [
+        0.1, 0.0, 0.1,  
+        0.9, 0.0, 0.1, 
+        0.9, 0.0, 0.9, 
+        0.1, 0.0, 0.9, 
+
+        0.5, 0.05, 0.5,
+
+
+        0.5, 0.05, 0.1,
+        0.5 + d, 0.05, 0.5 - d, 
+        0.9, 0.05, 0.5, 
+        0.5 + d, 0.05, 0.5 + d,   
+        0.5, 0.05, 0.9,    
+        0.5 - d, 0.05, 0.5 + d,   
+        0.1, 0.05, 0.5,    
+        0.5 - d, 0.05, 0.5 - d  
+    ];
+
+    objgl.bindBuffer(objgl.ARRAY_BUFFER, buf);
+    objgl.bufferData(objgl.ARRAY_BUFFER, new Float32Array(tab), objgl.STATIC_DRAW);
+    return buf;
+}
