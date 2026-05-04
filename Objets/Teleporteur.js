dbd4bc5a020b687e08438c9036864acd5ad542b1
Gogo <george.georgiev04@gmail.com>
2026-05-04 13:04:42 -0400

    creerMaillageTeleporteur

--- Objets/Teleporteur.js
+++ Objets/Teleporteur.js
@@ -16,25 +16,51 @@ function creerGeometrieTeleporteur(objgl) {
     const d = 0.283;
 
     let tab = [
-        0.1, 0.0, 0.1,  
-        0.9, 0.0, 0.1, 
-        0.9, 0.0, 0.9, 
-        0.1, 0.0, 0.9, 
+        0.1, 0.0, 0.1,
+        0.9, 0.0, 0.1,
+        0.9, 0.0, 0.9,
+        0.1, 0.0, 0.9,
 
         0.5, 0.05, 0.5,
 
 
         0.5, 0.05, 0.1,
-        0.5 + d, 0.05, 0.5 - d, 
-        0.9, 0.05, 0.5, 
-        0.5 + d, 0.05, 0.5 + d,   
-        0.5, 0.05, 0.9,    
-        0.5 - d, 0.05, 0.5 + d,   
-        0.1, 0.05, 0.5,    
-        0.5 - d, 0.05, 0.5 - d  
+        0.5 + d, 0.05, 0.5 - d,
+        0.9, 0.05, 0.5,
+        0.5 + d, 0.05, 0.5 + d,
+        0.5, 0.05, 0.9,
+        0.5 - d, 0.05, 0.5 + d,
+        0.1, 0.05, 0.5,
+        0.5 - d, 0.05, 0.5 - d
     ];
 
     objgl.bindBuffer(objgl.ARRAY_BUFFER, buf);
     objgl.bufferData(objgl.ARRAY_BUFFER, new Float32Array(tab), objgl.STATIC_DRAW);
+    return buf;
+}
+
+function creerMaillageTeleporteur(objgl) {
+    let buf = objgl.createBuffer();
+
+    let tabMaillage = [
+        0, 1, 2,
+        0, 2, 3,
+
+        4, 5, 6,
+        4, 6, 7,
+        4, 7, 8,
+        4, 8, 9,
+        4, 9, 10,
+        4, 10, 11,
+        4, 11, 12,
+        4, 12, 5 
+    ];
+
+    objgl.bindBuffer(objgl.ELEMENT_ARRAY_BUFFER, buf);
+    objgl.bufferData(objgl.ELEMENT_ARRAY_BUFFER, new Uint16Array(tabMaillage), objgl.STATIC_DRAW);
+
+    buf.intNbTriangles = 10; 
+    buf.intNbDroites = 0;
+
     return buf;
 }
